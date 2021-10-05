d408f6e62e9982b12f147ec6b40e96a3d6cdf793
Srinivas Varukala <svarukala@gmail.com>
2021-10-04 22:18:36 -0400

    first commit

---
+++ bot/messageExtensionBot.ts
@@ -0,0 +1,160 @@
+import { default as axios } from "axios";
+import * as querystring from "querystring";
+import { TeamsActivityHandler, CardFactory, TurnContext} from "botbuilder";
+
+export class MessageExtensionBot extends TeamsActivityHandler {
+
+  // Message Extension Code
+  // Action.
+  public async handleTeamsMessagingExtensionSubmitAction(
+    context: TurnContext,
+    action: any
+  ): Promise<any> {
+    switch (action.commandId) {
+      case "createCard":
+        return createCardCommand(context, action);
+      case "shareMessage":
+        return shareMessageCommand(context, action);
+      default:
+        throw new Error("NotImplemented");
+    }
+  }
+
+  // Search.
+  public async handleTeamsMessagingExtensionQuery(context: TurnContext, query: any): Promise<any> {
+    const searchQuery = query.parameters[0].value;
+    //https://www.abbreviations.com/abbr_api.php
+    const response = await axios.get(
+      `https://www.stands4.com/services/v2/abbr.php?${querystring.stringify({
+        term: searchQuery,
+        uid: "9400",
+        tokenid: "2KYeFFcCslBXAqOf",
+        format: "json"
+      })}`
+    );
+
+    const attachments = [];
+    const existingResults = [];
+    response.data.result.forEach((obj) => {
+      if(!existingResults.find(e=>e==obj.definition)){
+        existingResults.push(obj.definition);
+        const heroCard = CardFactory.heroCard(obj.term);
+        const preview = CardFactory.heroCard(obj.definition, obj.categoryname);
+        preview.content.tap = {
+          type: "invoke",
+          value: { name: obj.definition, description: obj.term +" | Category: "+ obj.categoryname },
+        };
+        const attachment = { ...heroCard, preview };
+        attachments.push(attachment);
+      }
+    });    
+    return {
+      composeExtension: {
+        type: "result",
+        attachmentLayout: "list",
+        attachments: attachments,
+      },
+    };
+  }
+
+  public async handleTeamsMessagingExtensionSelectItem(
+    context: TurnContext,
+    obj: any
+  ): Promise<any> {
+    return {
+      composeExtension: {
+        type: "result",
+        attachmentLayout: "list",
+        attachments: [CardFactory.heroCard(obj.name, obj.description)],
+      },
+    };
+  }
+
+  // Link Unfurling.
+  public async handleTeamsAppBasedLinkQuery(context: TurnContext, query: any): Promise<any> {
+    const attachment = CardFactory.thumbnailCard("Image Preview Card", query.url, [query.url]);
+
+    const result = {
+      attachmentLayout: "list",
+      type: "result",
+      attachments: [attachment],
+    };
+
+    const response = {
+      composeExtension: result,
+    };
+    return response;
+  }
+}
+
+async function createCardCommand(context: TurnContext, action: any): Promise<any> {
+  // The user has chosen to create a card by choosing the 'Create Card' context menu command.
+  const data = action.data;
+  const heroCard = CardFactory.heroCard(data.title, data.text);
+  heroCard.content.subtitle = data.subTitle;
+  const attachment = {
+    contentType: heroCard.contentType,
+    content: heroCard.content,
+    preview: heroCard,
+  };
+
+  return {
+    composeExtension: {
+      type: "result",
+      attachmentLayout: "list",
+      attachments: [attachment],
+    },
+  };
+}
+
+async function shareMessageCommand(context: TurnContext, action: any): Promise<any> {
+  // The user has chosen to share a message by choosing the 'Share Message' context menu command.
+  let userName = "unknown";
+  if (
+    action.messagePayload &&
+    action.messagePayload.from &&
+    action.messagePayload.from.user &&
+    action.messagePayload.from.user.displayName
+  ) {
+    userName = action.messagePayload.from.user.displayName;
+  }
+
+  // This Messaging Extension example allows the user to check a box to include an image with the
+  // shared message.  This demonstrates sending custom parameters along with the message payload.
+  let images = [];
+  const includeImage = action.data.includeImage;
+  if (includeImage === "true") {
+    images = [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtB3AwMUeNoq4gUBGe6Ocj8kyh3bXa9ZbV7u1fVKQoyKFHdkqU",
+    ];
+  }
+  const heroCard = CardFactory.heroCard(
+    `${userName} originally sent this message:`,
+    action.messagePayload.body.content,
+    images
+  );
+
+  if (
+    action.messagePayload &&
+    action.messagePayload.attachment &&
+    action.messagePayload.attachments.length > 0
+  ) {
+    // This sample does not add the MessagePayload Attachments.  This is left as an
+    // exercise for the user.
+    heroCard.content.subtitle = `(${action.messagePayload.attachments.length} Attachments not included)`;
+  }
+
+  const attachment = {
+    contentType: heroCard.contentType,
+    content: heroCard.content,
+    preview: heroCard,
+  };
+
+  return {
+    composeExtension: {
+      type: "result",
+      attachmentLayout: "list",
+      attachments: [attachment],
+    },
+  };
+}
